eea64c4448744d7e1116f0948a47bdf469b5826a
Dave Syer <dsyer@pivotal.io>
2020-01-09 05:30:12 -0500

    Get nginx working as proxy

--- controllers/spring_controller.go
+++ controllers/spring_controller.go
@@ -16,8 +16,12 @@ limitations under the License.
 package controllers
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
+	"strings"
+	"text/template"
 
 	"github.com/go-logr/logr"
 	apps "k8s.io/api/apps/v1"
@@ -44,6 +48,7 @@ type ProxyServiceReconciler struct {
 // +kubebuilder:rbac:groups=spring.io,resources=proxyservices,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=spring.io,resources=proxyservices/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=events,verbs=get;list;watch;create;update;patch;delete
 
@@ -92,8 +97,10 @@ func (r *ProxyServiceReconciler) createAndUpdateDeploymentAndService(req ctrl.Re
 	log := r.Log.WithValues("ProxyService", req.NamespacedName)
 
 	var services corev1.ServiceList
+	var configs corev1.ConfigMapList
 	var deployments apps.DeploymentList
 	var service *corev1.Service
+	var config *corev1.ConfigMap
 	var deployment *apps.Deployment
 
 	if err := r.List(ctx, &services, client.InNamespace(req.Namespace), client.MatchingFields{ownerKey: req.Name}); err != nil {
@@ -103,6 +110,76 @@ func (r *ProxyServiceReconciler) createAndUpdateDeploymentAndService(req ctrl.Re
 	if err := r.List(ctx, &deployments, client.InNamespace(req.Namespace), client.MatchingFields{ownerKey: req.Name}); err != nil {
 		log.Error(err, "Unable to list child Deployments")
 		return deployment, service, err
+	}
+	if err := r.List(ctx, &configs, client.InNamespace(req.Namespace), client.MatchingFields{ownerKey: req.Name}); err != nil {
+		log.Error(err, "Unable to list child ConfigMaps")
+		return deployment, service, err
+	}
+
+	log.Info("Found configs", "configs", len(configs.Items))
+	if len(configs.Items) == 0 {
+		var err error
+		config, err = r.constructConfig(&proxy)
+		if err != nil {
+			return deployment, service, err
+		}
+		if err := r.Create(ctx, config); err != nil {
+			log.Error(err, "Unable to create Service for proxy", "config", config)
+			return deployment, service, err
+		}
+
+		log.Info("Created Service for proxy", "config", config)
+		r.Recorder.Event(&proxy, corev1.EventTypeNormal, "ConfigMapCreated", "Created ConfigMap")
+	} else {
+		// update if changed
+		config = &configs.Items[0]
+		config = updateConfig(config, &proxy)
+		if err := r.Update(ctx, config); err != nil {
+			if apierrors.IsConflict(err) {
+				log.Info("Unable to update ConfigMap: reason conflict. Will retry on next event.")
+				err = nil
+			} else {
+				log.Error(err, "Unable to update ConfigMap for proxy", "config", config)
+				r.Recorder.Event(&proxy, corev1.EventTypeWarning, "ErrInvalidResource", fmt.Sprintf("Could not update Service: %s", err))
+			}
+			return deployment, service, err
+		}
+
+		log.Info("Updated ConfigMap for proxy", "config", config)
+		r.Recorder.Event(&proxy, corev1.EventTypeNormal, "ConfigMapUpdated", "Updated ConfigMap")
+	}
+
+	log.Info("Found services", "services", len(services.Items))
+	if len(services.Items) == 0 {
+		var err error
+		service, err = r.constructService(&proxy)
+		if err != nil {
+			return deployment, service, err
+		}
+		if err := r.Create(ctx, service); err != nil {
+			log.Error(err, "Unable to create Service for proxy", "service", service)
+			return deployment, service, err
+		}
+
+		log.Info("Created Service for proxy", "service", service)
+		r.Recorder.Event(&proxy, corev1.EventTypeNormal, "ServiceCreated", "Created Service")
+	} else {
+		// update if changed
+		service = &services.Items[0]
+		service = updateService(service, &proxy)
+		if err := r.Update(ctx, service); err != nil {
+			if apierrors.IsConflict(err) {
+				log.Info("Unable to update Service: reason conflict. Will retry on next event.")
+				err = nil
+			} else {
+				log.Error(err, "Unable to update Service for proxy", "service", service)
+				r.Recorder.Event(&proxy, corev1.EventTypeWarning, "ErrInvalidResource", fmt.Sprintf("Could not update Service: %s", err))
+			}
+			return deployment, service, err
+		}
+
+		log.Info("Updated Service for proxy", "service", service)
+		r.Recorder.Event(&proxy, corev1.EventTypeNormal, "ServiceUpdated", "Updated Service")
 	}
 
 	if len(deployments.Items) == 0 {
@@ -138,44 +215,22 @@ func (r *ProxyServiceReconciler) createAndUpdateDeploymentAndService(req ctrl.Re
 		r.Recorder.Event(&proxy, corev1.EventTypeNormal, "DeploymentUpdated", "Updated Deployment")
 	}
 
-	log.Info("Found services", "services", len(services.Items))
-	if len(services.Items) == 0 {
-		var err error
-		service, err = r.constructService(&proxy)
-		if err != nil {
-			return deployment, service, err
-		}
-		if err := r.Create(ctx, service); err != nil {
-			log.Error(err, "Unable to create Service for proxy", "service", service)
-			return deployment, service, err
-		}
-
-		log.Info("Created Service for proxy", "service", service)
-		r.Recorder.Event(&proxy, corev1.EventTypeNormal, "ServiceCreated", "Created Service")
-	} else {
-		// update if changed
-		service = &services.Items[0]
-		service = updateService(service, &proxy)
-		if err := r.Update(ctx, service); err != nil {
-			if apierrors.IsConflict(err) {
-				log.Info("Unable to update Service: reason conflict. Will retry on next event.")
-				err = nil
-			} else {
-				log.Error(err, "Unable to update Service for proxy", "service", service)
-				r.Recorder.Event(&proxy, corev1.EventTypeWarning, "ErrInvalidResource", fmt.Sprintf("Could not update Service: %s", err))
-			}
-			return deployment, service, err
-		}
-
-		log.Info("Updated Service for proxy", "service", service)
-		r.Recorder.Event(&proxy, corev1.EventTypeNormal, "ServiceUpdated", "Updated Service")
-	}
 	return deployment, service, nil
+}
+
+// Create a ConfigMap for the proxyservice application
+func (r *ProxyServiceReconciler) constructConfig(proxy *api.ProxyService) (*corev1.ConfigMap, error) {
+	config := createConfig(*proxy)
+	r.Log.Info("Config", "config", config)
+	if err := ctrl.SetControllerReference(proxy, config, r.Scheme); err != nil {
+		return nil, err
+	}
+	return config, nil
 }
 
 // Create a Deployment for the proxyservice application
 func (r *ProxyServiceReconciler) constructDeployment(proxy *api.ProxyService) (*apps.Deployment, error) {
-	deployment := createDeployment(proxy)
+	deployment := createDeployment(*proxy)
 	r.Log.Info("Deploying", "deployment", deployment)
 	if err := ctrl.SetControllerReference(proxy, deployment, r.Scheme); err != nil {
 		return nil, err
@@ -183,7 +238,7 @@ func (r *ProxyServiceReconciler) constructDeployment(proxy *api.ProxyService) (*
 	return deployment, nil
 }
 
-func createDeployment(proxy *api.ProxyService) *apps.Deployment {
+func createDeployment(proxy api.ProxyService) *apps.Deployment {
 	deployment := &apps.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Labels:    map[string]string{"proxy": proxy.Name},
@@ -203,6 +258,22 @@ func createDeployment(proxy *api.ProxyService) *apps.Deployment {
 						corev1.Container{
 							Name:  "nginx",
 							Image: "nginx",
+							VolumeMounts: []corev1.VolumeMount{
+								corev1.VolumeMount{
+									Name:      "nginx",
+									MountPath: "/etc/nginx",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						corev1.Volume{
+							Name: "nginx",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{Name: fmt.Sprintf("%s-config", proxy.Name)},
+								},
+							},
 						},
 					},
 				},
@@ -215,6 +286,70 @@ func createDeployment(proxy *api.ProxyService) *apps.Deployment {
 func updatePodTemplate(template *corev1.PodTemplateSpec, proxy *api.ProxyService) *corev1.PodTemplateSpec {
 	// TODO: reconcile services in proxy
 	return template
+}
+
+func createConfig(proxy api.ProxyService) *corev1.ConfigMap {
+	data := readMap("etc", proxy)
+	config := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-config", proxy.Name),
+			Namespace: proxy.Namespace,
+		},
+		Data: data,
+	}
+	return config
+}
+
+func readMap(path string, proxy api.ProxyService) map[string]string {
+	result := map[string]string{}
+	paths, _ := ioutil.ReadDir(path)
+	services := Services{
+		Upstreams: map[string]string{},
+		Mappings:  map[string]string{},
+	}
+	for count, service := range proxy.Spec.Services {
+		if count == 0 {
+			services.Mappings["default"] = service
+		} else {
+			services.Mappings[service] = service
+		}
+		services.Upstreams[service] = service
+	}
+	for _, file := range paths {
+		name := file.Name()
+		if strings.HasSuffix(name, ".tmpl") {
+			arr, err := ioutil.ReadFile(path + "/" + file.Name())
+			if err == nil {
+				value := string(arr)
+				value = strings.TrimSuffix(value, "\n")
+				var tmpl *template.Template
+				tmpl, err = template.New(file.Name()).Parse(value)
+				if err == nil {
+					buffer := &bytes.Buffer{}
+					err := tmpl.Execute(buffer, services)
+					if err == nil {
+						value := buffer.String() + "\n"
+						key := strings.TrimSuffix(name, ".tmpl")
+						result[key] = value
+					}
+				}
+			}
+		} else {
+			arr, _ := ioutil.ReadFile(path + "/" + name)
+			result[name] = strings.TrimSuffix(string(arr), "\n") + "\n"
+		}
+	}
+	return result
+}
+
+// Services a convenience wrapper for the nginx config template
+type Services struct {
+	Upstreams map[string]string
+	Mappings  map[string]string
+}
+
+func updateConfig(config *corev1.ConfigMap, proxy *api.ProxyService) *corev1.ConfigMap {
+	return config
 }
 
 func createService(proxy *api.ProxyService) *corev1.Service {
@@ -287,8 +422,26 @@ func (r *ProxyServiceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	}); err != nil {
 		return err
 	}
+	if err := mgr.GetFieldIndexer().IndexField(&corev1.ConfigMap{}, ownerKey, func(rawObj runtime.Object) []string {
+		// grab the service object, extract the owner...
+		config := rawObj.(*corev1.ConfigMap)
+		owner := metav1.GetControllerOf(config)
+		if owner == nil {
+			return nil
+		}
+		// ...make sure it's ours...
+		if owner.APIVersion != apiGVStr || owner.Kind != "ProxyService" {
+			return nil
+		}
+
+		// ...and if so, return it
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&api.ProxyService{}).
+		Owns(&corev1.ConfigMap{}).
 		Owns(&corev1.Service{}).
 		Owns(&apps.Deployment{}).
 		Complete(r)
---
+++ controllers/spring_controller_test.go
@@ -0,0 +1,83 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package controllers
+
+import (
+	api "github.com/dsyer/spring-service-operator/api/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+func TestCreateConfig(t *testing.T) {
+	proxy := api.ProxyService{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "demo",
+			Namespace: "test",
+		},
+		Spec: api.ProxyServiceSpec{
+			Services: []string{
+				"green",
+				"blue",
+			},
+		},
+	}
+	config := createConfig(proxy)
+	if config.Name != "demo-config" {
+		t.Errorf("config.Name = %s; want 'demo-config'", config.Name)
+	}
+	if len(config.Data) != 0 {
+		t.Errorf("config.Data = %d; want 0", len(config.Data))
+	}
+}
+
+func TestCreateMap(t *testing.T) {
+	proxy := api.ProxyService{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "demo",
+			Namespace: "test",
+		},
+		Spec: api.ProxyServiceSpec{
+			Services: []string{
+				"green",
+				"blue",
+			},
+		},
+	}
+	_, filename, _, _ := runtime.Caller(0)
+	config := readMap(filepath.Dir(filename)+"/../etc", proxy)
+	if len(config) != 3 {
+		t.Errorf("config.Data = %d; want 3", len(config))
+	}
+	if !strings.Contains(config["proxy.conf"], "upstream green") {
+		t.Errorf("config['proxy.conf'] = %s; want 'upstream green'", config["proxy.conf"])
+	}
+	if !strings.Contains(config["proxy.conf"], "server green") {
+		t.Errorf("config['proxy.conf'] = %s; want 'server green'", config["proxy.conf"])
+	}
+	if !strings.Contains(config["proxy.conf"], "server blue") {
+		t.Errorf("config['proxy.conf'] = %s; want 'server blue'", config["proxy.conf"])
+	}
+	if !strings.Contains(config["proxy.conf"], "upstream blue") {
+		t.Errorf("config['proxy.conf'] = %s; want 'upstream blue'", config["proxy.conf"])
+	}
+	for key, file := range config {
+		if !strings.HasSuffix(file, "\n") {
+			t.Errorf("%s; want newline", key)
+		}
+	}
+}
